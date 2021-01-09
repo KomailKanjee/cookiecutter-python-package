041970c5a5b18edb5b3bf1274ff746f70f528102
Komail Kanjee <kkanjee@eeng.pro>
2021-01-09 14:22:46 -0500

    Adding extras_require=docs to setup.py for cookiecutter project to setup.py

--- setup.py
+++ setup.py
@@ -36,12 +36,13 @@ setup(
     python_requires=">=3.6",
     install_requires=[''],
     extras_require={
+        "dev": [],
+        "docs": ["sphinx"],
         "test": [
             'cookiecutter', 'coverage', 'flake8', 'gitpython', 'pylint',
             'pytest', 'pytest-cookies', 'pytest-cov', 'pytest-xdist', 'pyyaml',
             'safety', 'tox', 'yapf'
-        ],
-        "dev": []
+        ]
     },
     zip_safe=False,
     platforms="any",
